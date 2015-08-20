bba02086f584d5331b847390c7d18466972a0103
ajeeshb4u <ajeeshb4u@gmail.com>
2015-08-20 15:20:50 +0530

    Serial (USART)
    UART by polling

---
+++ STM/serial 2/serial.d
@@ -0,0 +1,8 @@
+serial.o: Serial.c
+serial.o: C:\Keil\ARM\Inc\ST\STM32F10x\STM32F10x.h
+serial.o: C:\Keil\ARM\CMSIS\Include\core_cm3.h
+serial.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
+serial.o: C:\Keil\ARM\CMSIS\Include\core_cmInstr.h
+serial.o: C:\Keil\ARM\CMSIS\Include\core_cmFunc.h
+serial.o: C:\Keil\ARM\Inc\ST\STM32F10x\system_stm32f10x.h
+serial.o: Serial.h
